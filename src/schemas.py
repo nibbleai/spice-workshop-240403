26060a3f5d3ab0e31363403df47a1feb443ad250
Lucas Le Ray <lucas.a.leray@gmail.com>
2023-11-27 18:04:42 +0100

    Add all source columns in taxi schema

--- src/schemas.py
+++ src/schemas.py
@@ -6,12 +6,25 @@ from spice.utils import StrEnum
 class TaxiColumn(StrEnum):
     """Column names in the taxi dataset"""
 
+    VENDOR_ID = 'vendor_id'
     PICKUP_TIME = 'pickup_datetime'
     DROPOFF_TIME = 'dropoff_datetime'
+    PASSENGER_COUNT = 'passenger_count'
+    TRIP_DISTANCE = 'trip_distance'
     PICKUP_LON = 'pickup_longitude'
     PICKUP_LAT = 'pickup_latitude'
+    RATE_CODE = 'rate_code'
+    STORE_AND_FWD_FLAG = 'store_and_fwd_flag'
     DROPOFF_LON = 'dropoff_longitude'
     DROPOFF_LAT = 'dropoff_latitude'
+    PAYMENT_TYPE = 'payment_type'
+    FARE_AMOUNT = 'fare_amount'
+    EXTRA = 'extra'
+    MTA_TAX = 'mta_tax'
+    TIP_AMOUNT = 'tip_amount'
+    TOLLS_AMOUNT = 'tolls_amount'
+    IMP_SURCHARGE = 'imp_surcharge'
+    TOTAL_AMOUNT = 'total_amount'
 
 
 class WeatherColumn(StrEnum):
