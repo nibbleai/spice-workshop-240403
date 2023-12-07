b5aa504777b23713bdf8d56cf90dd0962f14ead5
Lucas Le Ray <lucas.a.leray@gmail.com>
2023-12-07 12:13:42 +0100

    Drop cross-validation

--- src/model.py
+++ src/model.py
@@ -1,12 +1,7 @@
-import pandas as pd
-
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_validate, TimeSeriesSplit
+from sklearn.metrics import mean_squared_log_error
 
 __all__ = ['get_model', 'evaluate']
-
-N_SPLITS = 5
-SCORING = 'neg_mean_squared_log_error'
 
 
 def get_model():
@@ -14,13 +9,5 @@ def get_model():
 
 
 def evaluate(model, *, features, target):
-    splitter = TimeSeriesSplit(n_splits=N_SPLITS)
-    cv_losses = cross_validate(
-        model, features, target, scoring=SCORING, cv=splitter,
-    )
-
-    return (
-        pd.DataFrame(cv_losses)
-        .rename(columns={'test_score': SCORING})
-        .agg(['mean', 'std'])
-    )
+    predicted = model.predict(features)
+    return {'mean_squared_log_error': mean_squared_log_error(target, predicted)}
