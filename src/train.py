4a029f04ff9f29d00a30e623408d35f932379433
Lucas Le Ray <lucas.a.leray@gmail.com>
2023-12-11 17:38:18 +0100

    Add base feature columns to extract

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ import logging
 from src.data import get_train_dataset, get_target, train_test_split
 from src.preprocessing import preprocess
 from src.model import get_model, evaluate
+from src.schemas import TaxiColumn
 
 
 def main():
@@ -14,8 +15,14 @@ def main():
     train_target = target.loc[train_data.index]
     test_target = target.loc[test_data.index]
 
-    train_features = ...  # TO IMPLEMENT
-    test_features = ...  # TO IMPLEMENT
+    feature_columns = [
+        TaxiColumn.PICKUP_LAT,
+        TaxiColumn.PICKUP_LON,
+        TaxiColumn.DROPOFF_LAT,
+        TaxiColumn.DROPOFF_LON,
+    ]
+    train_features = train_data.loc[:, feature_columns]
+    test_features = test_data.loc[:, feature_columns]
 
     logging.info("Training model...")
     model = get_model().fit(train_features, train_target)
